3ce9ec80611b1c900b3885e652d7b058f35e696d
Eric Oliveira <oiericoi@hotmail.com>
2025-11-09 10:48:34 -0300

    feat: add new user fields

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,11 @@
+// /internal/model/usuario.go
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 const (
 	RoleCliente = "cliente"
@@ -8,13 +13,20 @@ const (
 )
 
 type Usuario struct {
-	ID        uint      `gorm:"primaryKey"`
-	Nome      string    `gorm:"not null"`
-	Email     string    `gorm:"unique;not null"`
-	SenhaHash string    `gorm:"not null"`
-	Telefone  string
-	Endereco  string
-	Tipo      string    `gorm:"default:'cliente';not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID          uint   `gorm:"primaryKey"`
+	Nome        string `gorm:"not null"`
+	Email       string `gorm:"unique;not null"`
+	SenhaHash   string `gorm:"not null"`
+	Telefone    string `gorm:"size:20"`
+	CEP         string `gorm:"size:10"`
+	Rua         string `gorm:"size:255"`
+	Numero      string `gorm:"size:20"`
+	Complemento string `gorm:"size:100"`
+	Bairro      string `gorm:"size:100"`
+	Cidade      string `gorm:"size:100"`
+	Estado      string `gorm:"size:2"`
+	Tipo        string `gorm:"default:'cliente';not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
